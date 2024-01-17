0ef56098fac7114d88e5279c82d7ab497b216313
shivam-cashflo <kumar.shivam@cashflo.io>
2024-01-17 22:46:47 +0530

    added table

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,12 +1,9 @@
 import {useNavigate} from 'react-router-dom';
 import {useState, useEffect} from 'react';
 import axios from 'axios';
-import Signup from './Signup';
-
 
 const Welcome = () => {
     const navigate = useNavigate();
-    const data = false;
     const [apiData, setApiData] = useState([]);
 
     const getApiData = async() =>{
@@ -16,10 +13,20 @@ const Welcome = () => {
 
     useEffect(() =>{
         getApiData()
-    },[])
+    },[apiData])
 
     const handleSignupclick = () =>{
         navigate('/signup');
+    }
+
+    const handleEdit = async(id) =>{
+        const payload = {fullName:"Surya Kumar"}
+        try{
+            await axios.put(`http://127.0.0.1:3002/${id}`, payload);
+            getApiData()
+        }catch(error){
+            console.log(error)
+        }
     }
     
     return (
@@ -30,19 +37,36 @@ const Welcome = () => {
                 <div className="hero-body">
                     <div className="container">
                         <div className="has-text-centered">
-                            {apiData.map((item, index) =>{
+                        <table className='table is-fullwidth'>
+                    <thead>
+                        <tr>
+                        <th className='has-text-left'>Id</th> 
+                        <th className='has-text-left'>S.No</th>
+                        <th className='has-text-left'>Name</th>
+                        <th className='has-text-left'>Email</th>
+                        <th className='has-text-left'>Edit</th>
+                        <th className='has-text-left'>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {apiData.map((item, index) =>{
                                 return (
-                                    <div className='container' key={item.id}>
-                                        <h1 className='title'>{item.fullName}</h1>
-                                        <p className='subtitle'>{item.email}</p>
-                                        <h2>{item.id}</h2>
-                                    </div>
+                                    <tr key={item.id}>
+                                    <td className='has-text-left'>{index + 1}</td>
+                                    <td className='has-text-left'>{item.id}</td>
+                                    <td className='has-text-left'>{item.fullName}</td>
+                                    <td className='has-text-left'>{item.email}</td>
+                                    <td className='has-text-left'><button className='button is-warning' onClick={() =>{handleEdit(item.id)}}>Edit</button></td>
+                                    <td className='has-text-left'><button className='button is-danger'>Delete</button></td>
+                                    </tr>
                                 )
                             })}
-                            </div>
+                        </tbody>
+                </table>
                         </div>
                     </div>
                 </div>
+            </div>
         </section>
     )
 }
