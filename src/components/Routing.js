f5a21e605deafd75c540998f747a6ce15c1ee79f
shivam-cashflo <kumar.shivam@cashflo.io>
2024-01-24 19:12:40 +0530

    24/1/2024

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {lazy, Suspense} from "react";
 
+import ChartData from "./ChartData";
+
 const PostsList = lazy(() => import("./PostsList"));
 const Home = lazy(() => import("./Home"));
 const NavBar = lazy(() => import("./NavBar"))
@@ -26,6 +28,7 @@ const Routing = () =>{
                     <Route exact path="/about" element={<About/>}></Route>
                     <Route exact path="/counter" element={<Counter/>}></Route>
                     <Route exact path="/create/post" element={<CreatePost/>}></Route> 
+                    <Route exact path="/charts" element={<ChartData/>}></Route>
                 </Routes>
                 </Suspense>
             </Router>
